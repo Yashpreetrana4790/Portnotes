f44b8636ced0ebf475794bbb1709b954e62049f2
Yashpreetrana4790 <109335257+Yashpreetrana4790@users.noreply.github.com>
2024-09-01 12:13:57 +0530

    favicon update

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='text-3xl flex items-center justify-center'>
+    <div className='text-xl flex items-center justify-center'>
       <div>
         <h1>Projects</h1>
         <p>Projects are under construction.</p>
