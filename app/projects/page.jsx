956a974dd834f0bc50e1d7d0a6aff79023f5a514
Yashpreetrana4790 <109335257+Yashpreetrana4790@users.noreply.github.com>
2024-09-01 11:56:19 +0530

    change in layout fix

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const page = () => {
   return (
     <div className='text-3xl flex items-center justify-center'>
-      <h1>Projects</h1>
-      <p>Projects are under construction.</p>
+      <div>
+        <h1>Projects</h1>
+        <p>Projects are under construction.</p>
+      </div>
     </div>
   )
 }
